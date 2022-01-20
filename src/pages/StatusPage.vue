1ba8c8f5077d3b7a5eecd5a813093d94ac67c3cd
blackwhitebear8 <45701225+blackwhitebear8@users.noreply.github.com>
2022-01-20 19:57:49 +0100

    changed footer

--- src/pages/StatusPage.vue
+++ src/pages/StatusPage.vue
@@ -208,7 +208,7 @@
         </div>
 
         <footer class="mt-5 mb-4">
-            {{ $t("Powered by") }} <a target="_blank" href="https://github.com/louislam/uptime-kuma">{{ $t("Uptime Kuma" ) }}</a>
+            <a target="_blank" href="https://pixelhosting.nl/">{{ $t("PixelHosting" ) }}</a>
         </footer>
     </div>
 </template>
